bfc931c50d3ffe6d901829407157a1533506d7ef
Greg Samuelson <grssam@gmail.com>
2021-04-11 13:20:51 -0700

    update

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-let dateIn = parseInt(1565481600000)
-console.log(dateIn)
-var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-var date = new Date(dateIn); // Thu Apr 09 2020 14:28:32 GMT+0100 (British Summer Time)
-let year = date.getFullYear(); // 2020
-let month = date.getMonth() + 1; // 4 (note zero index: Jan = 0, Dec = 11)
-month = months[date.getMonth()]
-let day = date.getDate(); // 9
+async function getRSS() {
 
-console.log(date, month, day, year)
+await axios.get('https://beachvolleyblog.net/feed')
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => console.log(data))
+}
+  getRSS()
